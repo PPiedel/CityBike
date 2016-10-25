fe2db23dace5763699a275d595928a9801482697
PPiedel <pawelpiedel@yahoo.pl>
2016-10-25 20:07:51 +0200

    Previous button functionality added. Created "switchFromCurrentSectionTo" function.

--- src/js/registration.js
+++ src/js/registration.js
@@ -1,25 +1,47 @@
+
 $( document ).ready(function() {
     var sections = $('.form-section');
+    var step_wizards = $('.stepwizard-step');
+    console.log("Sections length : "+sections.length);
+    console.log("Steps length : "+step_wizards.length);
+
 
     $("#next").click(function () {
         navigateTo(curIndex() + 1);
     });
 
+    $("#previous").click(function () {
+        navigateTo(curIndex() - 1);
+    });
+
     function curIndex() {
-        var index = sections.index(sections.filter('.current'));
-        console.log("Current section index : " + index);
-        return index;
+        return index = sections.index(sections.filter('.current'));
     }
 
     function navigateTo(index) {
-        console.log("Next section index : " + index);
+        var previous = $('#previous');
+
+        console.log("index : " + index);
+        if (index==0){
+            if(! previous.hasClass( "disabled" )){
+                previous.addClass('disabled');
+            }
+            switchFromCurrentSectionTo(index)
+        }
+        else if (index<sections.length){
+            previous.removeClass('disabled');
+            console.log("Previous enabled");
+            switchFromCurrentSectionTo(index);
+        }
+    }
+
+    function switchFromCurrentSectionTo(index) {
         sections.filter('.current').hide();
         sections.removeClass('current').eq(index).addClass('current');
         sections.filter('.current').show();
-
     }
-});
 
+});
 
 
 
