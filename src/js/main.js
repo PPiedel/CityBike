82cfc291caa48df62f371c28b1c9a856198fe4b5
PPiedel <pawelpiedel@yahoo.pl>
2016-11-16 19:47:46 +0100

    Slideshow works fine, but it's position is still wrong.

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,35 @@
 var slides = document.querySelectorAll('#slides .slide');
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide,2000);
-var playing = true;
-var pauseButton = document.getElementById('pause');
+var next = document.getElementById('next');
+var previous = document.getElementById('previous');
+var controls = document.querySelectorAll('.controls');
 
-function nextSlide() {
+
+for(var i=0; i<controls.length; i++){
+    controls[i].style.display = 'inline-block';
+}
+
+next.onclick = function(){
+    nextSlide();
+};
+
+previous.onclick = function(){
+    previousSlide();
+};
+
+function nextSlide(){
+    goToSlide(currentSlide+1);
+}
+
+function previousSlide(){
+    goToSlide(currentSlide-1);
+}
+
+function goToSlide(n){
     slides[currentSlide].className = 'slide';
-    currentSlide = (currentSlide+1)%slides.length;
+    currentSlide = (n+slides.length)%slides.length;
     slides[currentSlide].className = 'slide showing';
 }
 
-function pauseSlideshow() {
-    pauseButton.innerHTML = 'Play';
-    playing = false;
-    clearInterval(slideInterval);
-}
 
-function playSlideshow() {
-    pauseButton.innerHTML = 'Pause';
-    playing = true;
-    slideInterval = setInterval(nextSlide,2000);
-}
-
-pauseButton.onclick = function() {
-    if(playing) {
-        pauseSlideshow();
-    } else {
-        playSlideshow();
-    }
-};
