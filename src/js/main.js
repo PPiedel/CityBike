163d676ca55a149f95d92528de9a511449912318
PPiedel <pawelpiedel@yahoo.pl>
2016-11-16 19:37:11 +0100

    Pause button added.

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,31 @@
 var slides = document.querySelectorAll('#slides .slide');
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide,2000);
+var playing = true;
+var pauseButton = document.getElementById('pause');
 
 function nextSlide() {
     slides[currentSlide].className = 'slide';
     currentSlide = (currentSlide+1)%slides.length;
     slides[currentSlide].className = 'slide showing';
 }
+
+function pauseSlideshow() {
+    pauseButton.innerHTML = 'Play';
+    playing = false;
+    clearInterval(slideInterval);
+}
+
+function playSlideshow() {
+    pauseButton.innerHTML = 'Pause';
+    playing = true;
+    slideInterval = setInterval(nextSlide,2000);
+}
+
+pauseButton.onclick = function() {
+    if(playing) {
+        pauseSlideshow();
+    } else {
+        playSlideshow();
+    }
+};
