116c61bf92e01a70aef5e81401e58514559b9195
PPiedel <pawelpiedel@yahoo.pl>
2016-11-22 23:31:38 +0100

    DOM functions for first two ex. from DOM list created.

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,20 @@
-var slides = document.querySelectorAll('#slides .slide');
+var slides = document.querySelectorAll("#slides .slide");
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide,4000);
-var next = document.getElementById('next');
-var previous = document.getElementById('previous');
-var controls = document.querySelectorAll('.controls');
+var next = document.getElementById("next");
+var previous = document.getElementById("previous");
+var controls = document.querySelectorAll(".controls");
+
+assignInlineBlockDisplay();
+createImage();
+countImages();
+countForms();
 
 
-for(var i=0; i<controls.length; i++){
-    controls[i].style.display = 'inline-block';
+function assignInlineBlockDisplay() {
+    for(var i=0; i<controls.length; i++){
+        controls[i].style.display = "inline-block";
+    }
 }
 
 next.onclick = function(){
@@ -27,22 +34,22 @@ function previousSlide(){
 }
 
 function goToSlide(n){
-    slides[currentSlide].className = 'slide';
+    slides[currentSlide].className = "slide";
     currentSlide = (n+slides.length)%slides.length;
-    slides[currentSlide].className = 'slide showing';
+    slides[currentSlide].className = "slide showing";
 }
 
 var playing = true;
-var pauseButton = document.getElementById('pause');
+var pauseButton = document.getElementById("pause");
 
 function pauseSlideshow() {
-    pauseButton.innerHTML = 'Play';
+    pauseButton.innerHTML = "Play";
     playing = false;
     clearInterval(slideInterval);
 }
 
 function playSlideshow() {
-    pauseButton.innerHTML = 'Pause';
+    pauseButton.innerHTML = "Pause";
     playing = true;
     slideInterval = setInterval(nextSlide,4000);
 }
@@ -55,3 +62,22 @@ pauseButton.onclick = function() {
     }
 };
 
+function createImage() {
+    var image = document.createElement("IMG");
+    image.setAttribute("src", "img/dynamic.jpg");
+    image.setAttribute("width", "700");
+    image.setAttribute("width", "470");
+    image.setAttribute("alt", "Rower");
+    document.getElementById("dynamicImageContainer").appendChild(image);
+
+}
+
+function countImages() {
+    var imagesNumber = document.images.length;
+    console.log("Images number : " + imagesNumber);
+}
+
+function countForms() {
+    var x = document.forms.length;
+    console.log("Forms number : " + x);
+}
