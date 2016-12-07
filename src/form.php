02bfa0b727a518d1ef5d9fc150e5ad0520ac79c4
PPiedel <pawel.piedel@yahoo.pl>
2016-12-07 19:30:12 +0100

    Form.php functions created.

--- src/form.php
+++ src/form.php
@@ -1,35 +1,50 @@
 <?php
 // define variables and set to empty values
-$name = $lastname = $email = $phone = $month = $message = $country = $gender = $whoAreYou ="";
+$surnameErr = $nameErr = $emailErr = $genderErr = $websiteErr = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") 
+$surname = $name = $email = $phone = $month = $message = $country = $gender = $whoAreYou ="";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
-  $name = test_input($_POST["name"]);
-  $lastname = test_input($_POST["lastname"]);
+  $name = test_name($_POST["surname"]);
+  $lastname = test_input($_POST["name"]);
   $email = test_input($_POST["email"]);
   $phone = test_input($_POST["phone"]);
-  $month = test_input($_POST["month"]);
-  $message = test_input($_POST["message"]);
-  $country = test_input($_POST["country"]);
-  $gender = test_input($_POST["gender"]);
-  $whoAreYou = test_input($_POST["whoAreYou"]);
-  
-  function test_input($data) 
-  {
-	  return $data;
+ 
+
+  function test_name($data){
+    if (!preg_match("/^[a-zA-Z ]*$/",$surname)) {
+    $nameErr = "Only letters and white space allowed";
+    }
   }
-  
-  /*
-if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-  $nameErr = "Only letters and white space allowed";
+
+  function test_last_name($data){
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+    $nameErr = "Only letters and white space allowed";
+    }
+  }
+
+  function test_email($data){
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $emailErr = "Invalid email format";
+    }
+  }
+
+  function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
 }
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-  $emailErr = "Invalid email format";
-}
+
+
+
+
+
 $website = test_input($_POST["website"]);
 if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
   $websiteErr = "Invalid URL";
 }
 }
-*/
+
 ?>
