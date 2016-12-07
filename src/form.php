62392fa7285a8cefd4d0c41acf3fd12f1486c258
Ernest Dziemidowicz <ernestdzi@interia.pl>
2016-12-07 20:30:47 +0100

    zbieranie informacji o miesiącu

--- src/form.php
+++ src/form.php
@@ -39,10 +39,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   }
   
   #phone
-    $phone = test_input($_POST["phone"]);
+    $phone = test_input($_POST["tel"]);
     if (!preg_match("/^[0-9 ]{9}$/",$phone)) {
       $phoneErr = "Numer telefonu musi się składać z 9 cyfr."; 
     }
+	
+	#month
+    $month = test_input($_POST["month"]);
   
   if (empty($_POST["message"])) {
     $messageErr = "Wiadomość jest pusta";
